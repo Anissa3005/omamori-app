796c5024b049c698c0ad8e6d033738671df67eac
Anissa Chadouli <Anissa3005>
2023-05-25 13:05:36 +0900

    feat: adding html for backend

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cors = require("cors");
+
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`Listen to port ${PORT}`);
