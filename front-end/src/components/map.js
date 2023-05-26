2390b8f9b343150bc504394deaecc8ee668e7ed0
Anissa Chadouli <Anissa3005>
2023-05-26 17:18:40 +0900

    feat:pins are working, i can also post on the

--- front-end/src/components/map.js
+++ front-end/src/components/map.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
 import "../styles/map.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet/dist/leaflet";
 
 export default function Map() {
+  const [omamori, setOmamori] = useState([]);
+
+  const customIcon = new Icon({
+    iconUrl:
+      "https://img.freepik.com/premium-vector/red-pin-point-isolated-white-background_120819-360.jpg?w=826",
+    iconSize: [38, 38],
+  });
+
+  useEffect(() => {
+    getOmamori();
+  }, [omamori]);
+
+  // useEffect(() => {
+  //   console.log([Number(omamori[0].latitude), Number(omamori[0].longitude)]);
+  // }, []);
+
+  async function getOmamori() {
+    const response = await fetch("http://localhost:8080/omamori");
+    const data = await response.json();
+    setOmamori(data);
+  }
+
   return (
     <div>
       <MapContainer center={[35.652832, 139.839478]} zoom={10}>
@@ -11,6 +34,12 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {omamori.map((marker) => (
+          <Marker
+            position={[Number(marker.latitude), Number(marker.longitude)]}
+            icon={customIcon}
+          ></Marker>
+        ))}
       </MapContainer>
     </div>
   );
